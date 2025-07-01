c2fc7e8fa5b43124885d6a9bc7acd92d0c9e9a61
minh huon <mhuon@atum.bio>
2025-07-01 14:44:12 -0700

    first push

---
+++ assets/js/5e95c892.ef163228.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgene_deisgner_wiki=self.webpackChunkgene_deisgner_wiki||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(6540);var n=r(4164),i=r(5500),u=r(7559),a=r(2831),c=r(1656),d=r(4848);function t(e){return(0,d.jsx)(i.e3,{className:(0,n.A)(u.G.wrapper.docsPages),children:(0,d.jsx)(c.A,{children:(0,a.v)(e.route.routes)})})}}}]);
